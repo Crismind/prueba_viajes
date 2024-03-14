728fd885bc7aada0da746e4c765dd653ed13894a
Crismind <cristobal.pinochet@gmail.com>
2024-03-13 22:56:23 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Smooth scroll para los enlaces de la barra de navegación
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
